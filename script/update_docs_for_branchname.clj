755d6190250d466a0292ac1beaa022b258b1b361
Bryan Maass <bryan.maass@gmail.com>
2025-04-29 13:31:09 -0600

    tweak commands in update_docs_for_branchname

--- script/update_docs_for_branchname.clj
+++ script/update_docs_for_branchname.clj
@@ -2,6 +2,7 @@
   (:require
    [babashka.process :as p]
    [bling.core :refer [bling callout]]
+   [clojure.string :as str]
    [clj-yaml.core :as yaml]
    [util :as u]))
 
@@ -13,46 +14,29 @@
   (let [[_ version-num] (re-matches #"v0.(\d+)" (:docs_version (yaml/parse-string (slurp "_config.yml"))))]
     (Integer/parseInt version-num)))
 
-(defn update? [config-docs-version release-num]
-  (cond
-    (> release-num config-docs-version)
-    (do
-      (callout {:type :warning
-                :label "Newer version detected"}
-               (format "Release number from branch (%s) is newer than docs_version from config.yml (%s).\nContinuing build."
-                       release-num
-                       config-docs-version))
-      false)
-
-    (= release-num config-docs-version)
-    true
-
-    :else false))
-
 (defn usage []
   (println "Usage: script/update_docs_for_branchname.clj branchname")
   (System/exit 1))
 
 (defn -main [& args]
-  (let [branchname (first args)
-        dry-run? (contains? (set args) "--dry-run")
-        _ (when (nil? branchname)
-            (println (callout {:type :error} (bling [:red "No branchname provided."])))
-            (usage))
-        [category release-num] (u/categorize-branchname branchname)
-        command (case category
-                  :master "./script/docs master --set-version master"
-                  :release (format "./script/docs release-x.%s.x --set-version v0.%s %s"
-                                   release-num
-                                   release-num
-                                   (if (update? (config-docs-version) release-num)
-                                     "--update"
-                                     ""))
-                  :test (format "./script/docs %s --set-version %s" branchname branchname)
-                  (do (println "Unpublishable branchname: " branchname)
-                      (System/exit 1)))]
-    (callout {:type :info :label "Command To Run"} (bling [:green command]))
-    (when-not dry-run? (p/shell command))))
+  (let [branchname    (first args)
+        dry-run?      (contains? (set args) "--dry-run")
+        _             (when (nil? branchname)
+                        (println (callout {:type :error} (bling [:red "No branchname provided."])))
+                        (usage))
+        [category
+         release-num] (u/categorize-branchname branchname)
+        command       (cond
+                        (= category :master)                  "./script/docs master --set-version master"
+                        ;; for "current version", just use docs-update
+                        (= (config-docs-version) release-num) "./script/docs-update"
+                        (= category :release)                 (format "./script/docs release-x.%s.x --set-version v0.%s" release-num release-num)
+                        (= category :test)                    (format "./script/docs %s --set-version %s" branchname branchname)
+                        :else (do (println "Unpublishable branchname: " branchname)
+                                  (System/exit 1)))]
+    (callout {:type :info :label (str "Command for " branchname)} command)
+    (when-not dry-run?
+      (p/shell command))))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (apply -main *command-line-args*))
