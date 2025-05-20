4fe8303e8d64e061089755bfe67cfd11d7a4677f
Bryan Maass <bryan.maass@gmail.com>
2025-05-20 11:21:41 -0600

    cleanup html checker script

--- script/_test/identical_html_checker.clj
+++ script/_test/identical_html_checker.clj
@@ -21,40 +21,56 @@
       (str/replace #"../metabase.github.io/" "")
       (str/replace #"_site/docs/" "")))
 
-(defn missing-file-report [subpath]
+(defn missing-file-report [other-repo-path subpath]
   (let [docs-pages (sort (map without-prefix
                               (html-files (str "_site/docs/" subpath))))
         og-pages (sort (into []
                              (map without-prefix)
-                             (html-files (str "../metabase.github.io/_site/docs/" subpath))))
-        diff (sort (remove (set docs-pages) og-pages))
-        ok? (zero? (count diff))]
+                             (html-files (str other-repo-path "/_site/docs/" subpath))))
+        [docs-only' og-only'] (d/diff (set docs-pages) (set og-pages))
+        docs-only (vec (sort docs-only'))
+        og-only   (vec (sort og-only'))
+        ok? (zero? (+ (count docs-only) (count og-only)))]
 
     {:ok? ok?
+
      :subpath subpath
+
+     :docs-only docs-only
+     :og-only   og-only
+
      :docs-pages docs-pages
-     :og-pages og-pages
-     :diff diff}))
+     :og-pages og-pages}))
 
 (defn -main [& args]
-  (let [reports (for [subpath (conj (map #(str "v0." %) (range 12 (inc (u/config-docs-version))))
-                                    "latest"
-                                    "master")]
-                  (missing-file-report subpath))]
-    (when-let [errors (seq (remove :ok? reports))]
-      (ice/p [:on-red "ERROR:"])
-      (doseq [report errors]
-        (let [{:keys [subpath docs-pages og-pages docs-pages diff]} report]
-          (println "Missing Files Report for subpath:" subpath)
-          (ice/p [:red "   >"] [:cyan "docs-pages:   "] (count docs-pages))
-          (ice/p [:red "   >"] [:cyan "og-pages:     "] (count og-pages))
-          (ice/p [:red "   >"] [:cyan "diff count:   "] (count diff))
-          (ice/p [:red "Diffs:"])
-          (doseq [d diff]
-            (ice/p [:yellow "     - " d])))
-        (println)))
+  (let [other-repo-path (first args)
+        _ (when-not other-repo-path
+            (throw (ex-info (ice/p-str [:red (str "Please provide the path to the original repo \n"
+                                                  "Usage: identical_html_checker.clj ../metabase.github.io")])
+                            {:babashka/exit 1})))
+        _ (when-not (fs/exists? other-repo-path)
+            (throw (ex-info (ice/p-str [:red (str "Invalid path \n"
+                                                  "Usage: identical_html_checker.clj <path>")])
+                            {:babashka/exit 1})))
+        num-subpaths (map #(str "v0." %) (range 12 (inc (u/config-docs-version))))
+        subpaths (into [] (conj num-subpaths "latest" "master"))
+        _ (apply ice/p [:magenta "Checking Subpaths: "]
+                 ((juxt first (constantly "-") last) num-subpaths) ", and latest and master")
+        reports (for [subpath subpaths] (missing-file-report other-repo-path subpath))
+        {ok true errors false} (group-by :ok? reports)]
+    (doseq [error errors]
+      (let [{:keys [subpath docs-only og-only og-pages docs-pages diff]} error]
+        (println "Missing Files Report for subpath:" subpath)
+        (ice/p [:red "   >"] [:cyan "docs-pages:   "] (count docs-pages))
+        (ice/p [:red "   >"] [:cyan "og-pages:     "] (count og-pages))
+        (ice/p [:red "   >"] [:cyan "diff count:   "] (count diff))
+        (ice/p [:red "   >"] [:cyan "docs-only:    "] docs-only)
+        (ice/p [:red "   >"] [:cyan "og-only:      "] og-only))
+      (println))
+    (ice/p [:on-red "ERROR:"])
+    (ice/p [:red (mapv :subpath errors)] "\n")
     (ice/p [:on-green "OK:"])
-    (u/pp-line (map :subpath (filter :ok? reports)))))
+    (ice/p [:green (mapv :subpath ok)])))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (apply -main *command-line-args*))
