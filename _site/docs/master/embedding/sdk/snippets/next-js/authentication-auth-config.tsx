84b8e1b73b4d6710ac2cc4ca149a39c7fbee6aff
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-08-14 00:52:04 +0300

    [auto] adding content to docs-embedding-comps->master (#447)
    Co-authored-by: Metabase Docs bot <metabase-bot@metabase.com>

--- _site/docs/master/embedding/sdk/snippets/next-js/authentication-auth-config.tsx
+++ _site/docs/master/embedding/sdk/snippets/next-js/authentication-auth-config.tsx
@@ -1,4 +1,4 @@
-import { defineMetabaseAuthConfig } from "@metabase/embedding-sdk-react";
+import { defineMetabaseAuthConfig } from "@metabase/embedding-sdk-react/nextjs";
 
 const authConfig = defineMetabaseAuthConfig({
   metabaseInstanceUrl: "https://metabase.example.com", // Required: Your Metabase instance URL
