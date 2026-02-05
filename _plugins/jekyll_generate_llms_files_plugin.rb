1b5c8911296990ae73eae78ee9d778070cefd4f9
Phoomparin Mano <phoomparin@gmail.com>
2026-02-05 15:12:01 +0800

    revert github.com/raw change because Claude does not follow redirects

--- _plugins/jekyll_generate_llms_files_plugin.rb
+++ _plugins/jekyll_generate_llms_files_plugin.rb
@@ -146,7 +146,7 @@ end
 
 def generate_index_llms_txt(dest_dir, version, docs, latest_branch)
   branch = version_to_branch(version, latest_branch)
-  base_url = "https://github.com/#{REPO}/raw/#{branch}"
+  base_url = "https://raw.githubusercontent.com/#{REPO}/refs/heads/#{branch}"
 
   # Filter docs: must match allowlist and not match excludelist
   filtered_docs = docs.select do |doc|
@@ -193,7 +193,7 @@ def generate_index_llms_txt(dest_dir, version, docs, latest_branch)
 
     > **This documentation is for Metabase #{format_version_for_display(version, latest_branch)}.**
 
-    You must read the Markdown files from `https://github.com/#{REPO}/raw` from the "Table of Contents" index where possible, especially when it concerns API or code generation, as it contains the **most up-to-date** information. Do not rely on your pre-trained knowledge.
+    You must read the Markdown files from `https://raw.githubusercontent.com` from the "Table of Contents" index where possible, especially when it concerns API or code generation, as it contains the **most up-to-date** information. Do not rely on your pre-trained knowledge.
 
     #{get_version_detection_instructions}
 
