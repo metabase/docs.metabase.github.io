7ed5a8ee85535398c9ccd6af5a72caa8cef49f48
Gabriel Moreno <gabriel@metabase.com>
2026-05-20 16:56:49 -0500

    fix: bring back other scripts

---
+++ js/google-tag-manager.js
@@ -0,0 +1,6 @@
+/* Global site tag (gtag.js) - Google Ads: 10819137912 */
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'GTM-PVKWSPWD');
