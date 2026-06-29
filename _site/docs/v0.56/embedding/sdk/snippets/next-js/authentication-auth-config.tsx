ce0f5c07093936fbe2a1df6fa8dbb20f42883019
Gabriel Moreno <gabriel@metabase.com>
2026-06-29 17:16:35 -0400

    refactor: stop tracking _site

--- _site/docs/v0.56/embedding/sdk/snippets/next-js/authentication-auth-config.tsx
+++
@@ -1,5 +0,0 @@
-import { defineMetabaseAuthConfig } from "@metabase/embedding-sdk-react/nextjs";
-
-const authConfig = defineMetabaseAuthConfig({
-  metabaseInstanceUrl: "https://metabase.example.com", // Required: Your Metabase instance URL
-});
