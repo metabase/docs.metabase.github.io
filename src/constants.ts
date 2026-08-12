4372acf5bdf7c134a1f98aa8dab8b067e0498ed9
Timur Mashara <timur@metabase.com>
2026-08-12 21:16:05 +0200

    update Unify script embedding

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,8 @@
 export const DOCS_SRC_ROOT = "_docs";
+
+export const UNIFY_ENABLED_PAGES = [
+  "/docs/latest/embedding/modular-embedding",
+  "/docs/latest/embedding/sdk/quickstart-with-sample-app",
+  "/docs/latest/embedding/sdk/quickstart-cli",
+  "/docs/latest/embedding/full-app-embedding-quick-start-guide",
+];
