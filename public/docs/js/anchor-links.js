20fe3270996f11f764c19628ad7e5552cc194918
Brad Anderson <brad@metabase.com>
2026-09-03 12:44:56 -0400

    fix old layout header and footer by reusing astro components + removes now unneeded code

--- public/docs/js/anchor-links.js
+++ public/docs/js/anchor-links.js
@@ -2,6 +2,5 @@ document.addEventListener('DOMContentLoaded', function () {
     anchors.options = {
         placement: 'left',
     };
-    anchors.add();
-    anchors.remove("#search-results li h3");
+    anchors.add(".MB-Page h1, .MB-Page h2, .MB-Page h3, .MB-Page h4, .MB-Page h5, .MB-Page h6");
 });
